fc5477e4df258449f12d9e07a1322eb566bf97e0
shruti gudi <shrutigudi@gmail.com>
2019-09-11 20:09:46 -0700

    displayed data

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const db = require('./db')
 const path = require('path')
+const dataLayer = db('products.json', () => {})
 
 const app = express()
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/api/products', async (req, res, next) => {
+  try {
+    const products = await dataLayer.findAll()
+    res.send(products)
+  }
+  catch(ex){
+    next(ex)
+  }
 })
 
 app.listen(3000, () => {
